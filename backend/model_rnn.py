d6c7e1cb6bdac53e6e7b8528a019537550ba83bf
frezy04 <lynxrf04@gmail.com>
2025-12-23 09:43:41 +0700

    Update model_rnn.py

--- backend/model_rnn.py
+++ backend/model_rnn.py
@@ -4,25 +4,42 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-# --- Dummy fungsi penerjemah Morse ke teks ---
+# fungsi penerjemah Morse ke teks ---
 def translate_morse(morse_code):
     morse_dict = {
+        # Huruf
         '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
         '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
         '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
         '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
         '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
-        '--..': 'Z', '-----': '0', '.----': '1', '..---': '2',
-        '...--': '3', '....-': '4', '.....': '5', '-....': '6',
-        '--...': '7', '---..': '8', '----.': '9'
+        '--..': 'Z',
+
+        # Angka
+        '-----': '0', '.----': '1', '..---': '2', '...--': '3',
+        '....-': '4', '.....': '5', '-....': '6',
+        '--...': '7', '---..': '8', '----.': '9',
+
+        # Tanda baca
+        '.-.-.-': '.',   
+        '--..--': ',',   
+        '..--..': '?',   
+        '-.-.--': '!',   
+        '---...': ':',   
+        '-...-': '=',    
+        '-..-.': '/',    
+        '-.--.': '(',    
+        '-.--.-': ')',   
+        '-....-': '-',   
+        '.-.-.': '+',    
     }
 
     words = morse_code.strip().split(" / ")
     translated_words = []
 
     for word in words:
-        letters = word.split(" ")
-        translated_word = "".join([morse_dict.get(letter, '?') for letter in letters])
+        letters = word.split()
+        translated_word = "".join(morse_dict.get(l, '?') for l in letters)
         translated_words.append(translated_word)
 
     return " ".join(translated_words)
